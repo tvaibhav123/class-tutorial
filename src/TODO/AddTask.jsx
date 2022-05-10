3d4f2e2e7af3b750e53798d9e14f09774601f104
Vaibhav Tiwari <tvaibhav1204@gmail.com>
2022-05-10 09:38:46 +0530

    child to parent comm

--- src/TODO/AddTask.jsx
+++ src/TODO/AddTask.jsx
@@ -1,59 +1,106 @@
-import React, { useState } from 'react';
-import ViewTask from './ViewTask';
+import React, { useState } from "react";
+import ViewTask from "./ViewTask";
 
 const AddTask = () => {
-    
-    const [tasks, setTasks] = useState([]);
-    const submitHandler = (event) => {
-        event.preventDefault();
-       
-        const task = {
-            title : event.target[0].value,
-            description: event.target[1].value,
-            assignedTo: event.target[2].value
-        }
-        setTasks([...tasks, task])
-        //setTasks([[],{title:"asdfa",description:"asdf",assignedTo:"asdf"}])
-        //console.log("Form", "title:",title," Description:",description, " AssignedTo:",assignedTo)
-    }
-    
-    return (
-        <div>
-            <h1>Add Task</h1>
-            <form onSubmit={submitHandler}>
-                <div>
-                    <label htmlFor="title">Title</label>
-                    <input type="text" name="title" id="title" />
-                </div>
-                <div>
-                    <label htmlFor="description">Description</label>
-                    <input type="text" name="description" id="description" />
-                </div>
-                <div>
-                    <label htmlFor="assignedTo">Assigned To</label>
-                    <input type="text" name="assignedTo" id="assignedTo" />
-                </div>
-                <div>
-                    <button type="submit">Add Task</button>
-                </div>
-            </form>
-            {/* <div>   
-                {task.title} -  {task.description} - {task.assignedTo}
-            </div> */}
-            
-            {
-                tasks.map((task) => {
-                    return (
-                        <ViewTask title={task.title} description={task.description} assignedTo={task.assignedTo}/>
-                    )
-                })
-            }
-            
-        </div>
-    );
-}
+   const [title, setTitle] = useState("");
+   const [description, setDescription] = useState("");
+   const [assignedTo, setAssignedTo] = useState("");
 
-//title   <inout element>
-//task Description <input element>
-//assignedTo 
+   const [tasks, setTasks] = useState([]);
+
+   const titleChangeHandler = (event) => {
+      setTitle(event.target.value);
+      //console.log(title)
+   };
+
+   const descriptionChangehandler = (event) => {
+      setDescription(event.target.value);
+   };
+
+   const assignedToChnageHandler = (event) => {
+      setAssignedTo(event.target.value);
+   };
+
+   const submitHandler = (event) => {
+      event.preventDefault();
+      const task = {
+         title: title,
+         description: description,
+         assignedTo: assignedTo,
+         complete: false
+      };
+
+      setTasks([...tasks, task]);
+      setTitle("");
+      setDescription("");
+      setAssignedTo("");
+   };
+
+   const taskCompleteHandler = (taskTitle, taskDescription, taskAssignedTo) => {
+       const taskWithCompletedInformation = tasks.map(task => {
+           if(task.title === taskTitle){
+               task.complete = true;
+           }
+           return task;
+       })
+       setTasks(taskWithCompletedInformation);
+   };
+   return (
+      <div>
+         <h1>Add Task</h1>
+         <form onSubmit={submitHandler}>
+            <div>
+               <label htmlFor="title">Title</label>
+               <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  value={title}
+                  onChange={titleChangeHandler}
+               />
+            </div>
+            <div>
+               <label htmlFor="description">Description</label>
+               <input
+                  type="text"
+                  name="description"
+                  id="description"
+                  value={description}
+                  onChange={descriptionChangehandler}
+               />
+            </div>
+            <div>
+               <label htmlFor="assignedTo">Assigned To</label>
+               <input
+                  type="text"
+                  name="assignedTo"
+                  id="assignedTo"
+                  value={assignedTo}
+                  onChange={assignedToChnageHandler}
+               />
+            </div>
+            <div>
+               <button type="submit">Add Task</button>
+            </div>
+         </form>
+         {tasks.map((task) => {
+            return (
+               <ViewTask
+                  title={task.title}
+                  description={task.description}
+                  assignedTo={task.assignedTo}
+                  complete = {task.complete}
+                  onCompleteTask={taskCompleteHandler}
+               />
+            );
+         })}
+      </div>
+   );
+};
+
+//TwoWay Binding
+// contorlled Components
+// clear the form
+// child to parent data
+
 export default AddTask;
--- src/TODO/ViewTask.jsx
+++ src/TODO/ViewTask.jsx
@@ -1,5 +1,8 @@
 
 const ViewTask = (props) => {
+    const taskComplete = () => {
+        props.onCompleteTask(props.title, props.description, props.assignedTo)
+    }
     return (
         <div>
             <h1>View Tasks</h1>
@@ -8,7 +11,9 @@ const ViewTask = (props) => {
                     <li>Title: {props.title}</li>
                     <li>Description: {props.description}</li>
                     <li>AssignedTo: {props.assignedTo}</li>
+                    <li>IsCompleted?: {props.complete.toString()}</li>
                 </ul>
+                <button onClick={taskComplete}>Complete Task</button>
             </div>
         </div>
     );
